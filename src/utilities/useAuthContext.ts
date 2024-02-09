c37cea8239965e72b18b9f037bb34bff8e22ec7f
joshuackeller <joshuackeller@gmail.com>
2024-02-09 00:12:42 -0700

    wip

--- src/utilities/useAuthContext.ts
+++
@@ -1,9 +0,0 @@
-import { useContext } from "react";
-import { AuthContext } from "../layout/AuthContextProvider";
-
-const useAuthContext = () => {
-  const context = useContext(AuthContext);
-  return context;
-};
-
-export default useAuthContext;
