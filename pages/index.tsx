b66f531c1ddb55ac2cdef1a40fdf61f0b78c3627
nathanchandler26 <64334357+nathanchandler26@users.noreply.github.com>
2024-02-14 18:40:18 -0700

    updates

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,5 +2,25 @@ import useGetOrganization from "@/src/requests/organizations/useGetOrganization"
 
 export default function Home() {
   const { data: organization } = useGetOrganization();
-  return <div>{organization?.title}</div>;
+  return <div>
+          {organization?.title}
+          <br></br>
+          {organization?.description}
+          <br></br>
+          {organization?.longDescription}
+          <br></br>
+          {organization?.bodyFont}
+          <br></br>
+          {organization?.headerFont}
+          <br></br>
+          {organization?.primaryColor}
+          <br></br>
+          {organization?.secondaryColor}
+          <br></br>
+          {organization?.accentColor}
+          <br></br>
+            <img src={organization?.logo?.url} alt="Organization Logo" />
+          <br></br>
+            {/* <img src={organization?.} alt="Organization Banner" /> */}
+        </div>;
 }
