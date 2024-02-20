69dc326a0efc5f75e24663ee99fdcf3af12c3f7f
emilyjensen12 <emily12jensen@gmail.com>
2024-02-20 07:43:50 -0700

    white label listings

---
+++ src/components/forms/applicationForm.tsx
@@ -0,0 +1,185 @@
+import { PageComponent } from "@/pages/_app";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { Skeleton } from "../shadcn/Skeleton";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "../shadcn/Form";
+import { Input } from "../shadcn/Input";
+import { Button } from "../shadcn/Button";
+import useAddApplication from "@/src/requests/applications/useAddApplication";
+import { useToast } from "../shadcn/use-toast";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+const formSchema = z.object({
+  firstName: z.string().optional(),
+  lastName: z.string().optional(),
+  phone: z.string().optional(),
+  email: z.string().optional(),
+  gender: z.string().optional(),
+  address: z.string().optional(),
+  city: z.string().optional(),
+  state: z.string().optional(),
+  zip: z.string().optional(),
+  usCitizen: z.boolean().optional(),
+  workVisa: z.boolean().optional(),
+  languages: z.string().optional(),
+  availableStartDate: z.string().optional(),
+  note: z.string().optional(),
+  relocate: z.boolean().optional(),
+});
+
+const ApplicationForm: PageComponent = () => {
+  const { mutate: addApplication, isPending } = useAddApplication();
+  const [open, setOpen] = useState<boolean>(false);
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      firstName: "",
+      lastName: "",
+      phone: "",
+      email: "",
+      gender: "",
+      address: "",
+      city: "",
+      state: "",
+      zip: "",
+      usCitizen: false,
+      workVisa: false,
+      relocate: false,
+      languages: "",
+      availableStartDate: "",
+      note: "",
+    },
+  });
+
+  const { toast } = useToast();
+
+  const onSubmit = (values: z.infer<typeof formSchema>) => {
+    addApplication(
+      { body: values },
+      {
+        onSuccess: () => {
+          setOpen(false);
+        },
+      }
+    );
+  };
+
+  return (
+    <div>
+      <div>
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)}>
+            <FormField
+              control={form.control}
+              name="firstName"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>First Name</FormLabel>
+                  <FormControl>
+                    <Input placeholder="" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="lastName"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Last Name</FormLabel>
+                  <FormControl>
+                    <Input placeholder="" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="gender"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Gender</FormLabel>
+                  <FormControl>
+                    <Input placeholder="" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="address"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Street Address</FormLabel>
+                  <FormControl>
+                    <Input placeholder="" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="city"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>City</FormLabel>
+                  <FormControl>
+                    <Input placeholder="" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="state"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>State</FormLabel>
+                  <FormControl>
+                    <Input placeholder="" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="usCitizen"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>U.S Citizen?</FormLabel>
+                  <FormControl>
+                    <input type="checkbox" />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <div className="flex justify-end mt-3">
+              <Button disabled={isPending} type="submit">
+                Add
+              </Button>
+            </div>
+          </form>
+        </Form>
+      </div>
+    </div>
+  );
+};
+
+export default ApplicationForm;
