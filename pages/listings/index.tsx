37258ded96511fc27df1bbee3b04b025871d3282
emilyjensen12 <emily12jensen@gmail.com>
2024-02-22 08:03:37 -0700

    application

--- pages/listings/index.tsx
+++ pages/listings/index.tsx
@@ -2,16 +2,13 @@ import { PageComponent } from "../_app";
 import { useState } from "react";
 import ListingsCards from "@/src/components/tables/ListingsCards";
 
-
-
 const ListingsPage: PageComponent = () => {
   const [open, setOpen] = useState<boolean>(false);
-
-
 
   return (
     <div>
       <div>
+        <h2>Job Listings</h2>
         <ListingsCards />
       </div>
     </div>
--- src/components/tables/ListingsCards.tsx
+++ src/components/tables/ListingsCards.tsx
@@ -20,20 +20,25 @@ const ListingsCards = () => {
 
   return (
     <>
-      {data?.map((data) => (
-        <Card key={data.id} onClick={() => handleRowClick(baseRoute, data.id)}>
-          <CardHeader>
-            <CardTitle>{data.jobTitle}</CardTitle>
-            <CardDescription>{data.jobDescription}</CardDescription>
-          </CardHeader>
-          <CardContent>
-            <p>{data.jobRequirements}</p>
-          </CardContent>
-          <CardFooter>
-            <p>{data.location}</p>
-          </CardFooter>
-        </Card>
-      ))}
+      <div className="grid grid-cols-3 gap-5">
+        {data?.map((data) => (
+          <Card
+            key={data.id}
+            onClick={() => handleRowClick(baseRoute, data.id)}
+          >
+            <CardHeader>
+              <CardTitle>{data.jobTitle}</CardTitle>
+              <CardDescription>{data.jobDescription}</CardDescription>
+            </CardHeader>
+            <CardContent>
+              <p>{data.jobRequirements}</p>
+            </CardContent>
+            <CardFooter>
+              <p>{data.location}</p>
+            </CardFooter>
+          </Card>
+        ))}
+      </div>
     </>
   );
 };
